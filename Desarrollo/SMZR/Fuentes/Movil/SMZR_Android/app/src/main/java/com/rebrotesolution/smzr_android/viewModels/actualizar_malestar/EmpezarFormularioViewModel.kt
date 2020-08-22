3187706eaede46caaac40bddf11bd4ca275587c7
munante <anthony.munante@unmsm.edu.pe>
2020-08-22 00:47:40 -0500

    sharedpreference en formulario y crear icono de aplicacion

--- Desarrollo/SMZR/Fuentes/Movil/SMZR_Android/app/src/main/java/com/rebrotesolution/smzr_android/viewModels/actualizar_malestar/EmpezarFormularioViewModel.kt
+++ Desarrollo/SMZR/Fuentes/Movil/SMZR_Android/app/src/main/java/com/rebrotesolution/smzr_android/viewModels/actualizar_malestar/EmpezarFormularioViewModel.kt
@@ -1,5 +1,7 @@
 package com.rebrotesolution.smzr_android.viewModels.actualizar_malestar
 
+import android.app.Activity
+import android.content.Context
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,7 +9,8 @@ import androidx.lifecycle.ViewModel
 import com.rebrotesolution.smzr_android.interfaces.ButtonAcceptHandler
 
 class EmpezarFormularioViewModel(
-    private var listener: ButtonAcceptHandler
+    private var listener: ButtonAcceptHandler,
+    private var activity: Activity
 ) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
@@ -16,10 +19,16 @@ class EmpezarFormularioViewModel(
     private val _text_button =  MutableLiveData<String>().apply {
         value = "Registrar Malestares"
     }
+
     val text: LiveData<String> = _text
     val text_button: LiveData<String> = _text_button
 
+    private val sharedPreferences = activity.getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
     fun onComenzarFormulario(v: View){
+        val save = sharedPreferences.edit()
+        save.putBoolean("OPEN_FORM",true)
         listener.clickOnButton()
+        save.apply()
     }
 }
