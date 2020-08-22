eef888699003985adae9572a33fee8d7c4372f5c
Cesar  Gutierrez <cesar.gutierrez15@unmsm.edu.pe>
2020-08-22 04:38:07 -0500

    Fix test

--- Desarrollo/SMZR/Fuentes/Backend/smzr/test/smzr_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SmzrWeb.PageControllerTest do
-  use SmzrWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Bienvenido al API de SMZR!"
-  end
-end
