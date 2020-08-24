aaf9e4925b8b2434ecfee35aad5d647b458e4a3f
Cesar  Gutierrez <cesar.gutierrez15@unmsm.edu.pe>
2020-08-24 02:47:17 -0500

    Fix version

--- Desarrollo/SMZR/Fuentes/Backend/smzr/mix.exs
+++ Desarrollo/SMZR/Fuentes/Backend/smzr/mix.exs
@@ -4,7 +4,7 @@ defmodule Smzr.MixProject do
   def project do
     [
       app: :smzr,
-      version: "0.2.4",
+      version: "0.3.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
