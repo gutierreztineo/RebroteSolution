98158408493d48644e23cfee92db44b63bd43af3
Cesar  Gutierrez <cesar.gutierrez15@unmsm.edu.pe>
2020-08-23 21:51:58 -0500

    force ssl

--- Desarrollo/SMZR/Fuentes/Backend/smzr/config/prod.exs
+++ Desarrollo/SMZR/Fuentes/Backend/smzr/config/prod.exs
@@ -20,7 +20,7 @@ config :smzr, SmzrWeb.Endpoint,
          cacertfile: "/etc/letsencrypt/live/smzr.makinap.com/chain.pem",
          transport_options: [socket_opts: [:inet6]]
        ],
-       #force_ssl: [hsts: true],
+       force_ssl: [hsts: true],
        cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
